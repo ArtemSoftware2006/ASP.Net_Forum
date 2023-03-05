918a498c6b2017bf36d13afa4bdd5a335bf515d3
Artem <artem.egorov.art2006@gmail.com>
2023-03-05 12:02:50 +0300

    Создал ShowAllNoteViewModel

--- ASP.Net_Forum.DAL/Migrations/20230305085924_ShortDiscrioption.Designer.cs
+++ ASP.Net_Forum.DAL/Migrations/20230305085924_ShortDiscrioption.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ASP.Net_Forum.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASP.Net_Forum.DAL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230305085924_ShortDiscrioption")]
+    partial class ShortDiscrioption
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ASP.Net_Forum.Domain/ViewModels/Note/ShowAllNoteViewModel.cs
+++ ASP.Net_Forum.Domain/ViewModels/Note/ShowAllNoteViewModel.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ASP.Net_Forum.Domain.Entity
+namespace ASP.Net_Forum.Domain.ViewModels.Note
 {
-	public class Note
+	public class ShowAllNoteViewModel
 	{
-		[Key]
-		public int Id { get; set; }
 		public string Title { get; set; }
 		public string ShortDiscription { get; set; }
-		public string Body { get; set; }
 		public DateTime DateCreated { get; set; }
 		public int UserId { get; set; }
-		public User User { get; set; }
 	}
 }
