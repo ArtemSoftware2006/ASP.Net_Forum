8a088c8bfad48b52a9ab15ed15a7636ee3ce91ba
Artem <artem.egorov.art2006@gmail.com>
2023-03-05 14:18:58 +0300

    Добавил INoteService
    * Переименовал в NoteViewModel

--- ASP.Net_Forum.Domain/ViewModels/Note/NoteViewModel.cs
+++ ASP.Net_Forum.Domain/ViewModels/Note/NoteViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ASP.Net_Forum.Domain.ViewModels.Note
 {
-	public class ShowAllNoteViewModel
+	public class NoteViewModel
 	{
 		public string Title { get; set; }
 		public string ShortDiscription { get; set; }
---
+++ Servises/Interfaces/INoteService.cs
@@ -0,0 +1,23 @@
+﻿using ASP.Net_Forum.Domain.Entity;
+using ASP.Net_Forum.Domain.Response;
+using ASP.Net_Forum.Domain.ViewModels.Note;
+using ASP.Net_Forum.Domain.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.Net_Forum.Service.Interfaces
+{
+	public interface INoteService
+	{
+		Task<BaseResponse<bool>> Edit(int id, NoteViewModel model);
+		Task<BaseResponse<IEnumerable<Note>>> GetAll();
+		Task<BaseResponse<Note>> Get(int id);
+		Task<BaseResponse<Note>> GetByLogin(string login);
+		Task<BaseResponse<Note>> GetByTitle(string title);
+		Task<BaseResponse<bool>> Delete(int id);
+		Task<BaseResponse<bool>> Create(NoteViewModel userViewModel);
+	}
+}
